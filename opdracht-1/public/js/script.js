b86728d548a6227284388b138450c2596caece42
Sam-Guliker <samguliker@hotmail.com>
2018-04-10 14:40:29 +0200

    init

--- opdracht-1/public/js/script.js
+++ opdracht-1/public/js/script.js
@@ -32,14 +32,6 @@ document.body.onclick = function() {
 }
 
 recognition.onresult = function(event) {
-  // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
-  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
-  // It has a getter so it can be accessed like an array
-  // The [last] returns the SpeechRecognitionResult at the last position.
-  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
-  // These also have getters so they can be accessed like arrays.
-  // The [0] returns the SpeechRecognitionAlternative at position 0.
-  // We then return the transcript property of the SpeechRecognitionAlternative object
 
   var last = event.results.length - 1;
   var color = event.results[last][0].transcript;
