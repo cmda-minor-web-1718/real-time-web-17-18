674ba245114a3618ecfa4d1d5599359c48dac87c
Alex van der wal <amvvanderwal@live.nl>
2018-04-10 15:00:58 +0200

    Real basic configuration for socket.io

--- app/index.js
+++ app/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 
+
 const app = express()
+
+
+
+
 
 var router = express.Router()
 
@@ -13,7 +18,17 @@ nunjucks.configure('templates', {
     express: app
 })
 
+var io = require('socket.io').listen(app.listen(3000, '0.0.0.0', () => console.log('running!')))
 
+io.on('connection', function(socket){
+    console.log('a user connectsded')
+})
+
+//For Tracking When User Connects:
+io.sockets.on("connection",function(socket){
+    //var socket is the socket for the client who has connected.
+    })
+ 
 app.get('/', function(request, response){
     response.render('index.html')
 })
@@ -22,4 +37,3 @@ app.get('/lol', function(request, response){
     response.render('index.html')
 })
 
-app.listen(3000, '0.0.0.0', () => console.log('running!'))
