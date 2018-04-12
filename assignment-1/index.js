442c985d7790d21d28f407fe85567c86eb98a3fe
VincentKempers <vincent.m.kempers@gmail.com>
2018-04-12 18:33:14 +0200

    working playlist editor

--- assignment-1/index.js
+++ assignment-1/index.js
@@ -16,9 +16,19 @@ const searches = [
   },
 ];
 
+// https://scotch.io/tutorials/use-ejs-to-template-your-node-application
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // Routes
 app.get('/', (req, res) => {
   res.render('index', { searches });
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  searches.push(req.body);
+  res.redirect('/');
 });
 
 // Start the server
