8774b3e283c6725ca2257c4f3ef2120ef0828ddc
VincentKempers <vincent.m.kempers@gmail.com>
2018-04-12 17:32:53 +0200

    use EJS

--- assignment-1/index.js
+++ assignment-1/index.js
@@ -1,15 +1,14 @@
 const app = require('express')();
 
+// https://scotch.io/tutorials/use-ejs-to-template-your-node-application
 app.set('view engine', 'ejs');
 
 // Routes
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+  res.render('index');
 });
 
 // Start the server
 app.listen(3000, () => {
   console.log('app is running on localhost:3000');
 });
-
-// https://scotch.io/tutorials/use-ejs-to-template-your-node-application
