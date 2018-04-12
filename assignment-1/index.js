1453ac8b8763a142bc7214cbe892146746838f4d
VincentKempers <vincent.m.kempers@gmail.com>
2018-04-12 17:22:42 +0200

    install ejs

--- assignment-1/index.js
+++ assignment-1/index.js
@@ -1,5 +1,7 @@
 const app = require('express')();
 
+app.set('view engine', 'ejs');
+
 // Routes
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
@@ -9,3 +11,5 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('app is running on localhost:3000');
 });
+
+// https://scotch.io/tutorials/use-ejs-to-template-your-node-application
