346552170df2810c6aecefc6791fe81c220e30e8
Cascuna <amvvanderwal@live.nl>
2018-04-12 18:58:02 +0200

    Added profanity checking, display of active rooms,online users & reworked the  interface

--- app/index.js
+++ app/index.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 
+
 const app = express();
 
 var path = require('path');
 var router = express.Router();
 
+var unified = require('unified');
+var english = require('retext-english');
+var stringify = require('retext-stringify');
+var profanities = require('retext-profanities');
+var report = require('vfile-reporter');
+
 let rooms = ['room1'];
 let users = {};
+
+unified = unified()
+.use(english)
+.use(profanities)
+.use(stringify)
+
+
+var test = unified.runSync(unified.parse('fuck this'));
+
 
 express.static('global');
 app.use(express.static(path.join(__dirname, '/static')));
@@ -19,26 +35,45 @@ nunjucks.configure('templates', {
 const server = app.listen(3000, '0.0.0.0', () => console.log('running!'));
 const io = require('socket.io')(server);
 
+function updateUserList(room) {
+    console.log(users[room])
+    io.to(room).emit('update userlist', {
+        activeUsers : users[room]
+    })
+}
+
+
+
 function joinRoom(socket, user, room) {
     socket.join(room)
     if (!users[room]) {
         users[room] = []
     };
+    socket.emit('setup user client', {
+        room: room
+    })
     socket.to(room).broadcast.emit('user joined', {
-        user: user
+        user: user, room:room 
     });
     users[room].push(user) 
-    console.log(users[room])
+    console.log(users)
+    updateUserList(room)
+    io.sockets.emit('update roomlist', {
+        rooms : users
+    })
 };
 
 function leaveRoom(socket, user, room){
     try {
-        socket.to(room).broadcast.emit('user leaved', {
+        socket.leave(room)
+        socket.to(room).broadcast.emit('user left', {
             user: user
         });
 
-        users[room] = users[room].filter(user => user !== user)
-        socket.leave(room)
+        users[room] = users[room].filter(u => u !== user)
+
+        updateUserList(room)
+
     } catch(e){
         console.log(e)
     }
@@ -59,31 +94,48 @@ io.on('connection', function(socket){
 
     socket.on('login temp user', function(data){
         socket.user = data.username;
-        socket.room = "room1";
+        socket.color = '#'+Math.floor(Math.random()*16777215).toString(16);
+        console.log(socket.color);
+        socket.room = "General";
         joinRoom(socket, socket.user, socket.room);
     });
   
 
     socket.on('new message', function(data) {
-        io.to(socket.room).emit('new_message', {
-          message: data.message,
-          user: socket.user
+        let cusswords = []
+        let actualMessage = data.message
+        console.log(data.message) 
+        var bla = unified.process(data.message, function(err, message) {
+            for(message of message.messages){
+                console.log(message.profanitySeverity)
+                if (message.profanitySeverity === 2){
+                actualMessage = actualMessage.replace(String(message.actual), '*'.repeat(String(message.actual).length))
+                cusswords.push(message.message)    
+            }       
+            }
+        }) 
+        console.log(cusswords)
+        if (cusswords.length > 0) {
+            socket.emit('profane message', {
+                cusswords: cusswords,
+                message: 'WATCH YOUR MOUTH YOUNG ONE',
+            });
+        }
+
+        io.to(socket.room).emit('new message', {
+          cusswords: [], 
+          message: actualMessage,
+          user: socket.user,
+          user_color: socket.color 
         });
     });
 
 
     socket.on('change room', function(data){
-      socket.to(socket.room).broadcast.emit('user left', {
-          user: socket.user
-      });
-      socket.leave(socket.room);
+      leaveRoom(socket, socket.user, socket.room);
       socket.room = data.room;
       joinRoom(socket, socket.user, socket.room);
     });
-
-    // socket.on('change_username', (data) => {
-    //     socket.username = data.username
-    // })
 })
  
 app.get('/', function(request, response){
--- app/static/js/site.js
+++ app/static/js/site.js
@@ -2,15 +2,21 @@
     const sendMessage = document.getElementById('send_message');
     const messageInput = document.getElementById('message_input');
     const chatWindow = document.getElementById('messages');
-
+    const userWindow = document.getElementById('users');
+    const roomsWindow = document.getElementById('rooms');
+    
     const changeRoom = document.getElementById('change_room');
     const roomInput = document.getElementById('room_input');
 
     const tempUser = document.querySelector(".tempUser");
     const loginTempUser = document.querySelector(".loginTempUser");
 
+    const currentRoom = document.querySelector("#current_room");
+    
+
+  
     var socket = io();
- 
+    
     loginTempUser.addEventListener("click", function(e) {
         socket.emit('login temp user', {
             username : tempUser.value
@@ -42,21 +48,74 @@
         chatWindow.appendChild(mItem)
     })
 
+    socket.on('setup user client', function(data) {
+        console.log('test')
+        currentRoom.innerHTML = 'Current room ' + data.room
+    })
+
     socket.on('user joined', function(data) {
         let mItem = document.createElement("li")
         let userJoined = document.createElement("p")
         userJoined.textContent = `${data.user} joined`   
         mItem.appendChild(userJoined)
         chatWindow.appendChild(mItem)
-
     })
 
+    socket.on('update roomlist', function(data){
+        roomsWindow.innerHTML = ''
+        let roomsTitle = document.createElement("p")
+        roomsTitle.textContent =  Object.keys(data.rooms).length + ' Rooms active'
+        roomsWindow.appendChild(roomsTitle)
+        for(room of Object.keys(data.rooms)){   
+            let roomItem = document.createElement("li")
+            let roomName = document.createElement("a")
+            roomName.textContent = `${room}`   
+            roomName.addEventListener('click', function(event) {
+                socket.emit('change room', {
+                    room : event.target.innerHTML
+                })
+            })
+            roomItem.appendChild(roomName)
+            roomsWindow.appendChild(roomItem)
+        }
+    })
+
+    socket.on('update userlist', function(data){
+        console.log(data.activeUsers)   
+        userWindow.innerHTML = ''
+        let usersTitle = document.createElement("p")
+        usersTitle.textContent = data.activeUsers.length + ' Online users'
+        userWindow.appendChild(usersTitle)
+        for(user of data.activeUsers){
+            let mItem = document.createElement("li")
+            let userJoined = document.createElement("p")
+            userJoined.textContent = `${user} `   
+            mItem.appendChild(userJoined)
+            userWindow.appendChild(mItem)
+           
+        }
+       
+    })
+
+    socket.on('profane message', function(data){
+        let mItem = document.createElement("li")
+        let p = document.createElement("p")
+
+        p.textContent = data.message + ' ' + data.cusswords
+        mItem.appendChild(p)
+        chatWindow.appendChild(mItem)
+    })
     socket.on("new message", function(data) {
         console.log(data.message)
         let mItem = document.createElement("li")
         let p = document.createElement("p")
-
-        p.textContent = data.user + ': ' + data.message
+        let userDiv = document.createElement("div")
+        userDiv.textContent = data.user + ' '
+        console.log(String(data.user_color))
+        userDiv.style.color = String(data.user_color)
+        p.appendChild(userDiv)
+        t = document.createTextNode(data.message);
+        p.appendChild(t)
         mItem.appendChild(p)
         chatWindow.appendChild(mItem)
 
