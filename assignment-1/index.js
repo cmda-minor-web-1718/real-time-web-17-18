43c393ebe276ed12734cfd233e15b4ebdbe534fc
VincentKempers <vincent.m.kempers@gmail.com>
2018-04-12 17:18:30 +0200

    burn socket.io

--- assignment-1/index.js
+++ assignment-1/index.js
@@ -1,45 +1,11 @@
 const app = require('express')();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
 
-// app.engine('view engine','pug');
-
-// current amount of users
-var usernames = {};
-var playlist = {};
-
-server.listen(3000, () => {
-  console.log('app is running on localhost:3000');
-});
-
+// Routes
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-io.on('connection', (socket) => {
-
-  socket.on('sendchat', function(data){
-    socket.emit('updatechat', socket.username, data);
-  });
-
-  socket.on('adduser', function(username){
-    socket.username = username;
-    usernames[username] = username;
-    socket.emit('updatechat', 'SERVER', 'you have connected')
-    socket.broadcast.emit('updatechat', 'SERVER', username + 'have connected')
-    io.sockets.emit('updateusers', usernames);
-  })
-
-  socket.on('chat message', function(msg){
-    io.emit('chat message', socket.username + ': ' + msg);
-  });
-
-  socket.on('disconnect', function(disconnectmsg) {
-    delete usernames[socket.username];
-    io.sockets.emit('updateusers', usernames);
-    socket.broadcast.emit('updatechat','SERVER',socket.username + " has disconnected");
-  });
+// Start the server
+app.listen(3000, () => {
+  console.log('app is running on localhost:3000');
 });
-
-
-// maak een array om data te plaatsen.
